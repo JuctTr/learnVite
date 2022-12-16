c0f87c183299f6671887b9f7dcd28b66efed7da2
JuctTr <1301842163@qq.com>
2022-12-16 09:41:25 +0800

    feat(🤡): vite相关配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // publicDir: "../public",
+  // // 手动指定项目根目录位置
+  // root: path.join(__dirname, "src"),
+  plugins: [react()], // 配置了官方的 react 插件，来提供 React 项目编译和热更新的功能。
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
