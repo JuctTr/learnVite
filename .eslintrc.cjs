1266a8d43e93eb3a0d6720fb02ac06845bc8891f
JuctTr <1301842163@qq.com>
2022-12-17 00:23:22 +0800

    feat(prettier): 引入prettier，但不加入eslint文件

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,46 @@
+/**
+ * 参考文档：https://cloud.tencent.com/developer/article/1840432
+ */
 module.exports = {
     env: {
+        /**
+         * "writable"或者 true，表示变量可重写；
+         * "readonly"或者false，表示变量不可重写；
+         * "off"，表示禁用该全局变量。
+         */
         browser: true,
-        es2021: true,
+        es2021: true
     },
     extends: [
         "eslint:recommended",
         "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended"
+        // 1. 接入 prettier 的规则
+        // "prettier",
+        // "plugin:prettier/recommended"
     ],
     overrides: [],
     parser: "@typescript-eslint/parser",
     parserOptions: {
         ecmaFeatures: {
-            jsx: true,
+            jsx: true
         },
         ecmaVersion: "latest",
-        sourceType: "module",
+        sourceType: "module"
     },
+    // plugins: ["react", "@typescript-eslint", "prettier"],
     plugins: ["react", "@typescript-eslint"],
     rules: {
+        // 3. 注意要加上这一句，开启 prettier 自动修复的功能
+        // "prettier/prettier": "error",
         "linebreak-style": ["error", "unix"],
         quotes: ["error", "double"],
         semi: ["error", "always"],
-        "react/react-in-jsx-scope": "off",
+        "react/react-in-jsx-scope": "off"
     },
+    settings: {
+        react: {
+            version: "detect"
+        }
+    }
 };
--- vite.config.ts
+++ vite.config.ts
@@ -9,32 +9,32 @@ const variablePath = normalizePath(path.resolve("./src/variable.scss"));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // publicDir: "../public",
-  // // 手动指定项目根目录位置
-  // root: path.join(__dirname, "src"),
-  plugins: [react()], // 配置了官方的 react 插件，来提供 React 项目编译和热更新的功能。
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    // publicDir: "../public",
+    // // 手动指定项目根目录位置
+    // root: path.join(__dirname, "src"),
+    plugins: [react()], // 配置了官方的 react 插件，来提供 React 项目编译和热更新的功能。
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url))
+        }
     },
-  },
-  css: {
-    modules: {
-      // name 表示文件名 local表示类名，我们可以自定义元素的类名
-      generateScopedName: "[name]__[local]__[hash:base64:5]",
-    },
-    preprocessorOptions: {
-      scss: {
-        // 会在每一个scss文件中的开头注入
-        additionalData: `@import "${variablePath}";`,
-      },
-    },
-    postcss: {
-      plugins: [
-        autoprefixer({
-          overrideBrowserslist: ["Chrome > 20", "ff > 10", "ie 11"],
-        }),
-      ],
-    },
-  },
+    css: {
+        modules: {
+            // name 表示文件名 local表示类名，我们可以自定义元素的类名
+            generateScopedName: "[name]__[local]__[hash:base64:5]"
+        },
+        preprocessorOptions: {
+            scss: {
+                // 会在每一个scss文件中的开头注入
+                additionalData: `@import "${variablePath}";`
+            }
+        },
+        postcss: {
+            plugins: [
+                autoprefixer({
+                    overrideBrowserslist: ["Chrome > 20", "ff > 10", "ie 11"]
+                })
+            ]
+        }
+    }
 });
