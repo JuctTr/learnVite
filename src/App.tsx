3ed0ae704072df12fa98a5103f556f064725ce15
JuctTr <1301842163@qq.com>
2022-12-17 15:32:16 +0800

    feat(json): json文件的加载

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,19 @@ import style from "./index.module.scss";
 // 1. 导入图片
 import logoSrc from "@assets/vite.svg";
 // SVG 组件方式加载
-import { ReactComponent as ReactLogo } from "@assets/vite.svg";
+import { ReactComponent as SvgComponentLogo } from "@assets/vite.svg";
+// Vite 中已经内置了对于 JSON 文件的解析，底层使用@rollup/pluginutils 的 dataToEsm 方法将 JSON 对象转换为一个包含各种具名导出的 ES 模块
+import { version } from "../package.json";
 
 function App() {
     const [count, setCount] = useState(0);
     useEffect(() => {
-        const img = document.getElementById("logo") as HTMLImageElement;
+        const img = document.getElementById("vite-logo") as HTMLImageElement;
         img.src = logoSrc;
     }, []);
     return (
         <div className="App">
+            <h1>版本号：{version}</h1>
             <div>
                 <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
                     <img src="/vite.svg" className="logo" alt="Vite logo" />
@@ -43,10 +46,10 @@ function App() {
             </p>
             <h1>测试图片资源路径</h1>
             <img src={logoSrc} alt="" />
-            <img alt="" id="logo" />
-            <div className="logo" style={{ width: 20, height: 20 }}></div>
+            <img alt="" id="vite-logo" />
+            <div className="vite-logo" style={{ width: 20, height: 20 }}></div>
             <h1>SVG 组件方式加载</h1>
-            <ReactLogo />
+            <SvgComponentLogo />
         </div>
     );
 }
